2dd368675e3ede9e47c6db48ce7c81a7f0270f05
Corey Reed <reed.corey@gmail.com>
2024-03-18 22:12:59 -0400

    Implement ML pipeline - complete train_model.py script

--- ml/model.py
+++ ml/model.py
@@ -119,9 +119,11 @@ def performance_on_categorical_slice(
 
     """
     X_slice, y_slice, _, _ = process_data(
-        data,
+        # for input data, use data in column given as "column_name", with the slice_value
+        data[data[column_name] == slice_value],
         categorical_features=categorical_features,
         label=label,
+        # use training = False
         training=False,
         encoder=encoder,
         lb=lb
